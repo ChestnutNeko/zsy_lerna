3cae96d479822bb9203f5b13ee9980d17173fe32
zhanshiyu <zhanshiyu@jd.com>
2021-12-20 14:43:13 +0800

    cat

---
+++ packages/canvas-demo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <cat />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import cat from 'vuepress-plugin-cat/cat'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    cat
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
