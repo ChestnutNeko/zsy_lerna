0e838dba0c45f2be5cfe0a12dcc475924dfb5b4e
zhanshiyu <zhanshiyu@jd.com>
2021-12-21 18:58:19 +0800

    update

---
+++ packages/my-live/src/virtualGuide/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="virtual-guide">
+        <div class="guide-animation"></div>
+        <div class="guide-tips"></div>
+    </div>
+</template>
+
+<script>
+import CanvasKeyFrames from "canvaskeyframes";
+
+</script>
+
+<style lang="sass" scoped>
+.virtual-guide {
+    .guide-animation {
+
+    }
+    .guide-tips {}
+}
+</style>
